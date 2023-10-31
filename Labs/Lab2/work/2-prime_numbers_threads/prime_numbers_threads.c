ff6e8794bae0164e2115f23dc30f1b877810bcc7
Etienne <etiennecor2410@gmail.com>
2023-10-31 19:00:42 +0100

    Parallelisation de l'exo 2 et modification du makefile pour les tests

--- Labs/Lab2/work/2-prime_numbers_threads/prime_numbers_threads.c
+++ Labs/Lab2/work/2-prime_numbers_threads/prime_numbers_threads.c
@@ -10,68 +10,96 @@
 typedef struct
 {
   int thread_id;
+  int start;
+  int end;
 } thread_args_t;
 
 int total_prime = 0;
+pthread_mutex_t mutex;
 
 // Implement is_prime function
 // If the argument is a prime number function returns 1
 // If the argument is NOT a prime number function returns 0
-int is_prime(int num){
-    if (num % 2 == 0){
-        return 0; // 2 et tous les nombres divisables par 2 ne sont pas premiers
-    }
+int is_prime(int num)
+{
+  if (num <= 1)
+  {
+    return 0;
+  }
+  if (num == 2 || num == 3)
+  {
+    return 1; // 2 et 3 sont premiers
+  }
+  if (num % 2 == 0 || num % 3 == 0)
+  {
+    return 0; // Tous les nombres pairs et divisibles par 3 ne sont pas premiers
+  }
 
-    if (num == 3){
-      return 1; // 3 est premier
+  for (int i = 5; i * i <= num; i += 6)
+  { 
+    if (num % i == 0 || num % (i + 2) == 0)
+    { 
+        return 0;
     }
+  }
 
-    for (int i = 5; i * i <= num; i += 6) { // on détermine la racine carré du nombre
-        if (num % i == 0 || num % (i + 2) == 0) { // On vérifie les diviseurs jusqu'à la racine carré obtenu et on teste si c'est premier ou non.
-            return 0;
-        }
-    }
-
-    return 1;
+  return 1;
 }
 
 
 void *thread_runner(void* thread_arg)
 {
   thread_args_t *my_args = (thread_args_t *)(thread_arg);
+  int count = 0;
 
   printf("Thread %d start\n", my_args->thread_id);
   
-  // TODO: count the number of prime numbers
+  for (int num = my_args->start; num <= my_args->end; ++num)
+  {
+    if (is_prime(num))
+    {
+      count++;
+    }
+  }
+
+  printf("%d prime numbers in thread %d\n", count, my_args->thread_id);
+  pthread_mutex_lock(&mutex);
+  total_prime += count;
+  pthread_mutex_unlock(&mutex);
   
   printf("Thread %d stop\n", my_args->thread_id);
-  return (void *)0;
+  pthread_exit(NULL);
 }
 
 int main(int argc, char **argv)
 {
-  int numThreads = NUM_THREADS;  // Dedault value
-  pthread_t threads[numThreads];
-  thread_args_t threadParams[numThreads];
-
-  //read the number of threads
-  if(argc > 1)
+  int numThreads = NUM_THREADS;   // Dedault value 
+  if(argc > 1)                    //read the number of threads
   {
     numThreads = atoi(argv[1]);
     printf("Number of threads : %d\n", numThreads);
   }
+  pthread_t threads[numThreads];
+  thread_args_t threadParams[numThreads];
 
-  for (int i = 0; i < numThreads; i++) 
+  for (int i = 0; i < numThreads; i++)
   {
     threadParams[i].thread_id = i;
+    threadParams[i].start = i * (N / numThreads) + 1;
+    threadParams[i].end = (i + 1) * (N / numThreads);
+    if (i == numThreads - 1)
+    {
+      threadParams[i].end = N;
+    }
     pthread_create(&threads[i], NULL, thread_runner, (void *)&threadParams[i]);
   }
 
-
-  for (int i = 0; i < NUM_THREADS; ++i) 
+  for (int i = 0; i < numThreads; ++i) 
   {
     pthread_join(threads[i], NULL);
   }
+
+  pthread_mutex_destroy(&mutex);
 
   printf("NUMBER PRIMES = %d\n", total_prime);
   exit(EXIT_SUCCESS);
